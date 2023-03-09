2c84f1da1929783f4fb3e58004d146bc7ed2c946
Michael Hill <mhill421@gmail.com>
2023-03-08 18:17:56 -0800

    wip

--- capital_connection_profit.py
+++ capital_connection_profit.py
@@ -3,9 +3,9 @@ import ui
 
 class CapitalConnectionProfitView(ui.View):
     def __init__(self):
-        self.frame = (0, 0, 300, 300)
+        self.frame = (0, 0, 150, 150)
         self.background_color = "#ffffff"
-        self.border_width = 1
+        self.border_width = 3
         self.border_color = "#000000"
 
 
