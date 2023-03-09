6281283bb9508f53f29223cad9cca87eb56d5938
Michael Hill <mhill421@gmail.com>
2023-03-08 18:06:27 -0800

    wip

--- capital_connection_profit.py
+++ capital_connection_profit.py
@@ -4,6 +4,7 @@ import ui
 class CapitalConnectionProfitView(ui.View):
     def __init__(self):
         self.frame = (0, 0, 323, 397)
+        self.background_color = "#ffffff"
 
 
 CapitalConnectionProfitView().present("fullscreen")
