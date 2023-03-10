bd50634f7109b6d3b58ef622be814ed84faa009d
Michael Hill <logicbomb421@users.noreply.github.com>
2023-03-10 12:40:05 -0800

    added road cost

--- home.py
+++ home.py
@@ -1,17 +1,23 @@
 import ui
 
+road_cost_per_tile = 1
 
 @ui.in_background
 def calculate(sender):
     cap_city_citizens_field = sender.superview["cap_city_citizen_field"]
     conn_city_citizens_field = sender.superview["conn_city_citizen_field"]
-    result_label = sender.superview["result_label"]
+    num_tiles_field = sender.superview['num_tiles_field']
+    gross_yield_label = sender.superview["gross_yield_label"]
+    net_yield_label = sender.superview['net_yield_label']
 
     cap_city_citizens = float(cap_city_citizens_field.text)
     conn_city_citizens = float(conn_city_citizens_field.text)
-    result = (conn_city_citizens * 1.1) + (cap_city_citizens * 0.15) - 1
-    result_label.text = str(result)
+    gross_yield = (conn_city_citizens * 1.1) + (cap_city_citizens * 0.15) - 1
+    net_yield = gross_yield - (float(num_tiles_field.text) * road_cost_per_tile)
+    gross_yield_label.text = str(round(gross_yield, 2))
+    net_yield_label.text = str(round(net_yield, 2))
 
 
 v = ui.load_view("home")
+v.size_to_fit()
 v.present("fullscreen")
