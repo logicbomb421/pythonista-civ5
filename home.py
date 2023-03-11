cef715a05be9299b492934c046a8459c297b348d
Michael Hill <mhill421@gmail.com>
2023-03-10 16:27:57 -0800

    wip

--- home.py
+++ home.py
@@ -2,7 +2,26 @@ import ui
 from capital_connection_profit import CapitalConnectionProfitView
 
 
-class NavigationView(ui.NavigationView):
+# class NavigationView(ui.NavigationView):
+#     # __capital_city_connection_yield_button = None
+
+#     # @property
+#     # def _capital_city_connection_yield_button(self):
+#     #     if not self.__capital_city_connection_yield_button:
+#     #         self.__capital_city_connection_yield_button = ui.Button(
+#     #             name="cap_city_conn_button",
+#     #             title="Capital City Connection Yield",
+#     #             font=(SYSTEM_FONT, 15),
+#     #             border_width=1,
+#     #             corner_radius=5,
+#     #             action=lambda sender: self.push_view(CapitalConnectionProfitView()),
+#     #         )
+
+#     def __init__(self):
+#         self.add_subview(self._capital_city_connection_yield_button)
+
+
+class MainView(ui.View):
     __capital_city_connection_yield_button = None
 
     @property
@@ -14,16 +33,11 @@ class NavigationView(ui.NavigationView):
                 font=(SYSTEM_FONT, 15),
                 border_width=1,
                 corner_radius=5,
-                action=lambda sender: self.push_view(CapitalConnectionProfitView()),
+                action=lambda sender: self.navigation_view.push_view(CapitalConnectionProfitView()),
             )
 
     def __init__(self):
         self.add_subview(self._capital_city_connection_yield_button)
 
 
-class MainView(ui.View):
-    def __init__(self):
-        self.add_subview(NavigationView())
-
-
-MainView().present("fullscreen")
+ui.NavigationView(MainView()).present("fullscreen")
